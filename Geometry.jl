355feed0d7a8fe76de61c97f769305938a552fd9
Lloyd Dilley <ldilley@users.noreply.github.com>
2022-08-23 00:31:35 -0400

    Add geometry module

--- Geometry.jl
+++ Geometry.jl
@@ -24,12 +24,24 @@ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
-module Polymath
+module Geometry
 
-include("Geometry.jl")
+const PI = 3.141592653589793
 
-using .Geometry: PI, diameter, radius, area_circle, area_ellipse, area_rectangle, area_square, area_trapezoid, area_triangle
+diameter(radius) = radius * 2
 
-export PI, diameter, radius, area_circle, area_ellipse, area_rectangle, area_square, area_trapezoid, area_triangle
+radius(diameter) = diameter / 2
+
+area_circle(radius) = PI * radius ^ 2
+
+area_ellipse(major_semiaxis, minor_semiaxis) = PI * major_semiaxis * minor_semiaxis
+
+area_rectangle(base, height) = base * height
+
+area_square(side_length) = side_length ^ 2
+
+area_trapezoid(base_length, top_length, height) = (base_length + top_length) / 2 * height
+
+area_triangle(base, height) = 0.5 * base * height
 
 end
