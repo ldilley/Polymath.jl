decf7471b0068d9831cf8a5ec6a2386e725fcceb
Lloyd Dilley <ldilley@users.noreply.github.com>
2022-08-25 00:05:03 -0400

    Add make.jl and index.md

---
+++ docs/make.jl
@@ -0,0 +1,16 @@
+push!(LOAD_PATH, "../src/")
+using Documenter, Polymath
+
+makedocs(
+  modules = [Polymath],
+  sitename = "Polymath",
+  repo = "github.com/ldilley/polymath.git",
+  pages = [
+    "Home" => "index.md",
+    "Geometry" => "geometry.md"
+  ]
+)
+
+deploydocs(
+  repo = "github.com/ldilley/polymath.git"
+)
