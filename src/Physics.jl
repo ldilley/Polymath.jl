1e69d649c82045531427bf7eca3ae83c46f14c71
Lloyd Dilley <ldilley@users.noreply.github.com>
2022-09-08 22:44:55 -0400

    Add physics module

---
+++ src/Physics.jl
@@ -0,0 +1,68 @@
+"""
+Polymath - A mathematics library (https://github.com/ldilley/Polymath.jl)
+Copyright (c) 2022 Lloyd Dilley
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+
+module Physics
+
+"""
+    density(mass, volume)
+
+Calculate density (ρ or D) given the mass (m) and volume (V).
+
+# Example
+```julia-repl
+julia> density(55, 40)
+1.375
+```
+"""
+density(mass, volume) = mass / volume
+
+"""
+    mass(density, volume)
+
+Calculate mass (m) given the density (ρ or D) and volume (V).
+
+# Example
+```julia-repl
+julia> mass(1.375, 40)
+55.0
+```
+"""
+mass(density, volume) = density * volume
+
+"""
+    volume(density, mass)
+
+Calculate volume (V) given the density (ρ or D) and mass (m).
+
+# Example
+```julia-repl
+julia> volume(1.375, 55)
+40.0
+```
+"""
+volume(density, mass) = mass / density
+
+end
